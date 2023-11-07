51ca86975b107d6daac46f8ab3c4d7ad0c857042
Dek Arya <arya.satyad123@gmail.com>
2023-11-07 21:32:08 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\API\AnakAsuhController;
 
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');;
 
 Route::apiResource('anak-asuh', AnakAsuhController::class);
 
